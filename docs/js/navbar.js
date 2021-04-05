df35b35f719b732bac3abd20a7ce943dc2654b6c
AndyNic <andynic.summoners.war@gmail.com>
2021-04-04 21:44:21 -0400

    docs actualizado

---
+++ docs/js/navbar.js
@@ -0,0 +1,8 @@
+// Ham Burger
+
+const hamBurgern = document.querySelector(".ham-burger");
+
+hamBurgern.addEventListener("click", function(){
+
+    document.querySelector(".barra-de-navegacion").classList.toggle("show");
+});
